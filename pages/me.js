d05d28436652c0e7c7860d15b044dae95e878381
youngbloodcyb <cameron.youngblood@gmail.com>
2023-02-07 21:46:30 -0700

    add nav component

--- pages/me.js
+++ pages/me.js
@@ -1,10 +1,12 @@
 import Skills from "@/components/skills"
 import Intro from "@/components/intro"
+import Nav from "@/components/nav"
 
 export default function Me() {
     return (
         <>
             <main className="w-screen h-screen">
+                <Nav/>
                 <Skills/>
                 <Intro/>
             </main>
