54340cceb6fdb35af884576c0d134027d8d06df5
youngbloodcyb <cameron.youngblood@gmail.com>
2023-02-07 21:34:26 -0700

    add intro component

---
+++ components/intro.js
@@ -0,0 +1,23 @@
+export default function Intro() {
+    return (
+        <section className="w-full rounded-xl border-solid border-black border-2 bg-gray">
+            <div className="flex flex-row items-start justify-start w-full py-10">
+                <div className="w-1/3 mx-6">
+                    <div className="font-mono uppercase">
+                        <p>React is a powerful JavaScript library created by Facebook for building user interfaces. It enables developers to create reusable components, which can be used to quickly build dynamic and interactive user interfaces. React makes it easy to create high-performance websites and mobile applications, and its features and components make it an ideal choice for creating web and mobile applications. With React, developers can create web and mobile applications with a single codebase, and its powerful features enable developers to create complex applications with minimal effort.</p>
+                    </div>
+                </div>
+                <div className="w-1/3 mx-6">
+                    <div className="font-mono uppercase">
+                        <p>React is designed to be easy to use and understand, with its intuitive syntax and components. It is also highly extensible, allowing developers to customize and build upon the core library to create powerful applications. React also provides an extensive set of tools for debugging and testing applications, making it a great choice for teams of all sizes. With its performance and scalability, React is a great choice for creating both web and mobile applications.</p>
+                    </div>
+                </div>
+                <div className="w-1/3 mx-6">
+                    <div className="font-mono uppercase">
+                        <p>React is also highly versatile, allowing developers to quickly develop custom components and tools to suit their individual needs. It also integrates easily with other popular frameworks and libraries, making it easy to create complex applications with a single codebase. React also provides comprehensive documentation and a vibrant community of developers who are always willing to help and share their knowledge. With its performance, scalability, and versatility, React is an ideal choice for creating powerful and interactive web and mobile applications.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
--- pages/me.js
+++ pages/me.js
@@ -1,10 +1,12 @@
 import Skills from "@/components/skills"
+import Intro from "@/components/intro"
 
 export default function Me() {
     return (
         <>
             <main className="w-screen h-screen">
                 <Skills/>
+                <Intro/>
             </main>
         </>
     )
