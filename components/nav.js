1f995e77602399cd8d582486c0b798ecc77b34b1
youngbloodcyb <cameron.youngblood@gmail.com>
2023-02-07 22:31:26 -0700

    add locomotive scroll

--- components/nav.js
+++ components/nav.js
@@ -1,19 +1,19 @@
 export default function Nav() {
     return (
-        <header class="py-6 flex justify-between items-center font-roboto bg-gray uppercase rounded-b-xl border-solid border-black border-2">
-            <div class="font-grid text-xl transition-all duration-300 ease-in-out hover:font-extrabold hover:text-2xl mx-6 text-blue-400">
+        <header className="py-6 flex justify-between items-center font-roboto bg-gray uppercase rounded-b-xl border-solid border-black border-2 max-h-[78px]">
+            <div className="font-grid text-xl transition-all duration-300 ease-in-out hover:font-extrabold hover:text-2xl mx-6 text-blue-400">
                 <a href="/">cameron</a>
             </div>
             <nav className="mx-6 font-mono">
-                <ul class="flex items-center gap-6">
+                <ul className="flex items-center gap-6">
                     <li>
-                        <a href="/me" class="text-xl transition-all duration-300 ease-in-out hover:font-extrabold hover:text-2xl">me</a>
+                        <a href="/me" className="text-xl transition-all duration-300 ease-in-out hover:font-extrabold hover:text-2xl">me</a>
                     </li>
                     <li>
-                        <a href="/work" class="text-xl transition-all duration-300 ease-in-out hover:font-extrabold hover:text-2xl">work</a>
+                        <a href="/work" className="text-xl transition-all duration-300 ease-in-out hover:font-extrabold hover:text-2xl">work</a>
                     </li>
                     <li>
-                        <a href="/blog" class="text-xl transition-all duration-300 ease-in-out hover:font-extrabold hover:text-2xl">blog</a>
+                        <a href="/blog" className="text-xl transition-all duration-300 ease-in-out hover:font-extrabold hover:text-2xl">blog</a>
                     </li>
                 </ul>
             </nav>
--- pages/me.js
+++ pages/me.js
@@ -1,14 +1,27 @@
 import Skills from "@/components/skills"
 import Intro from "@/components/intro"
 import Nav from "@/components/nav"
+import { useLocomotiveScroll } from 'react-locomotive-scroll';
 
 export default function Me() {
+    const { scroll } = useLocomotiveScroll();
+
     return (
         <>
-            <main className="w-screen h-screen">
+            <main>
                 <Nav/>
-                <Skills/>
-                <Intro/>
+                <div className="z-0 flex justify-center items-center text-center font-grid w-full h-[800px]" data-scroll data-scroll-speed="-8">
+                    <div className="mx-36">
+                        <h1 className="text-8xl text-blue-400">i am a creative developer studying computer science</h1>
+                    </div>
+                </div>
+                <div data-scroll data-scroll-speed="0"><Skills/></div>
+                <div className="flex justify-center items-center text-center font-grid w-full h-[500px]" data-scroll data-scroll-speed="4">
+                    <div className="mx-36">
+                        <h1 className="text-8xl text-blue-400">i am a creative developer studying computer science</h1>
+                    </div>
+                </div>
+                <div data-scroll data-scroll-speed="0"><Intro/></div>
             </main>
         </>
     )
