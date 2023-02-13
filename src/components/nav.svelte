0cdcdbe5ed34e1cc1ba35fcdb9dc63a26225654c
Cameron Youngblood <95453018+youngbloodcyb@users.noreply.github.com>
2023-02-12 22:10:39 -0700

    Revert "2.0"

---
+++ src/components/nav.svelte
@@ -0,0 +1,18 @@
+<header class="py-6 w-10/12 flex justify-between items-center font-roboto">
+    <div class="text-xl transition-all duration-300 ease-in-out hover:text-green hover:font-extrabold hover:text-2xl">
+        <a href="/">cameron</a>
+    </div>
+    <nav>
+        <ul class="flex items-center gap-6">
+            <li>
+                <a href="/me" class="text-xl transition-all duration-300 ease-in-out hover:text-green hover:font-extrabold hover:text-2xl">me</a>
+            </li>
+            <li>
+                <a href="/work" class="text-xl transition-all duration-300 ease-in-out hover:text-green hover:font-extrabold hover:text-2xl">work</a>
+            </li>
+            <li>
+                <a href="/blog" class="text-xl transition-all duration-300 ease-in-out hover:text-green hover:font-extrabold hover:text-2xl">blog</a>
+            </li>
+        </ul>
+    </nav>
+</header>
